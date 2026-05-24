c9b7c6a35e194ddbfc8826548d4f1932220b58a7
Costi Yadav <costiyadav@gmail.com>
2026-05-25 00:20:05 +0530

    day-1 completed

--- 02-CodeFlow-and-CallStack.js
+++ 02-CodeFlow-and-CallStack.js
@@ -1,11 +1,64 @@
-function a() {
-    var x = 10
-    console.log(x)
-}
-function b() {
-    var x = 20
-    console.log(x)
-}
-console.log(a)
-console.log(b)
-console.log(x)
+// console.log("A");
+// function test() {
+//     console.log("B");
+// }
+// test();
+// console.log("C");
+
+// <---------------------------diff question-------------------------->
+
+// function outer() {
+//     console.log("Inside outer");
+//     inner();
+//     console.log("Back in outer");
+// }
+
+// function inner() {
+//     console.log("Inside inner");
+// }
+
+// outer();
+// <---------------------------diff question-------------------------->
+
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b() {
+//     var x = 20;
+//     console.log(x);
+// }
+
+// a();
+// b();
+// console.log(x);
+// <---------------------------diff question-------------------------->
+
+// function a() {
+//     var x = 10
+//     console.log(x)
+// }
+// function b() {
+//     var x = 20
+//     console.log(x)
+// }
+// console.log(a)
+// console.log(b)
+// console.log(x)
+// <---------------------------diff question-------------------------->
+// function first() {
+//     second();
+//     console.log("First done");
+// }
+
+// function second() {
+//     third();
+//     console.log("Second done");
+// }
+
+// function third() {
+//     console.log("Third done");
+// }
+
+// first();
