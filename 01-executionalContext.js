3c67dfa23e1f8dd2d0c99caae99c0a10c381c0f1
Costi Yadav <costiyadav@gmail.com>
2026-05-24 23:53:27 +0530

    first commit

---
+++ 01-executionalContext.js
@@ -0,0 +1,24 @@
+// JavaScript contains -> Executional Context
+// 
+// executive context have two parts :
+// --> environment variable
+// --> thread of execution 
+
+// Environmet variable contains:
+//--> key value pairs of different variables which are defined 
+
+// thread of execution :
+// --> executes peice of code one by one 
+
+//-------------------------< nature of javascript >-------------------
+
+//# Javascript is a synchronous single threaded language
+// --> meaning: javascript runs a single line at a time in a specific order
+
+// -------------------------< proving by code >-----------------------
+
+console.log("first")
+console.log("second")
+console.log("third")
+
+// hence these three lines run one by one we can say that javascirpt is a specific order
---
+++ 02-CodeFlow-and-CallStack.js
@@ -0,0 +1,11 @@
+function a() {
+    var x = 10
+    console.log(x)
+}
+function b() {
+    var x = 20
+    console.log(x)
+}
+console.log(a)
+console.log(b)
+console.log(x)
